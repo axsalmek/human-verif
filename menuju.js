8687e6c56c412a396996026ac12f304479a99e9c
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2023-11-08 18:31:01 +0700

    Update menuju.js

--- menuju.js
+++ menuju.js
@@ -121,7 +121,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://axsblog.xyz/2022/05/03/how-to-make-plank-strong-for-long/?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://trandingworldnow.blogspot.com/2022/05/how-to-make-plank-strong-for-long.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
